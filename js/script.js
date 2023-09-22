5cc091ec82cc5463ef60de7d216ae0f1b132aa11
gvmotta <gustavo.motta.cardoso.dev@gmail.com>
2023-09-22 18:27:26 -0300

    adding teachers section

--- js/script.js
+++ js/script.js
@@ -121,3 +121,23 @@ document.querySelector('#form2').addEventListener('submit', handleSubmit);
 function hidePopup(){      
   document.getElementById("popup_container").style.display="none";      
 }
+
+(function ($) {
+  // Acc
+    $(document).on("click", ".naccs .menu div", function() {
+      var numberIndex = $(this).index();
+
+      if (!$(this).is("active")) {
+          $(".naccs .menu div").removeClass("active");
+          $(".naccs ul li").removeClass("active");
+
+          $(this).addClass("active");
+          $(".naccs ul").find("li:eq(" + numberIndex + ")").addClass("active");
+
+          var listItemHeight = $(".naccs ul")
+            .find("li:eq(" + numberIndex + ")")
+            .innerHeight();
+          $(".naccs ul").height(listItemHeight + "px");
+        }
+    });
+  })(window.jQuery);
