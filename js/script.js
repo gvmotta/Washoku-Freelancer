2a827fe6cdcf40c7edd9801bc4b535d4acd01e41
gvmotta <gustavo.motta.cardoso.dev@gmail.com>
2023-09-25 20:23:53 -0300

    translating to english

--- js/script.js
+++ js/script.js
@@ -130,24 +130,4 @@ const carouselLinks = document.querySelectorAll(".carousel-link");
              $("#carouselExampleControls").carousel(slideIndex);
          });
      });
-
-     var modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('slick-slide00');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-console.log(img, modal, modalImg, captionText);
-function opa(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
+ 
